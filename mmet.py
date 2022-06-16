681a3232f9ad2ce8643d16dd4d0390112df8b7fc
xxtg666 <68174188+xxtg666@users.noreply.github.com>
2022-06-16 19:01:40 +0800

    v1.0.5
    · 添加 忽略ias.json 的提示
    · 修改部分界面文本
    · 可复制错误日志
    · 选择时不会再显示 versions 文件夹内非游戏版本的文件

--- mmet.py
+++ mmet.py
@@ -11,6 +11,8 @@ import getpass
 import zipfile
 import webbrowser as wb
 import traceback
+import pyperclip
+import platform
 
 
 def make_zip(dirpath, outFullName):
@@ -52,8 +54,8 @@ class mmet(QMainWindow, Ui_MainWindow):
 
     def on_pb_s1(self):
         self.versions_path = QFileDialog.getExistingDirectory(
-            self, "选择 versions 文件夹", os.getcwd())
-        if self.versions_path[-8:-1] == "version":
+            self, "选择 versions 文件夹")
+        if self.versions_path[-8:-1]+self.versions_path[-1] == "versions":
             self.pb_s1.setText(self.versions_path)
             self.groupBox.setEnabled(False)
             self.groupBox_2.setEnabled(True)
@@ -62,8 +64,11 @@ class mmet(QMainWindow, Ui_MainWindow):
 
     def on_pb_s2(self):
         versions_list = os.listdir(self.versions_path)
+        for i in versions_list:
+            if not os.path.isdir(self.versions_path+os.sep+i):
+                versions_list.remove(i)
         version = QInputDialog.getItem(
-            self, "选择游戏版本", '请选择一个将导出的游戏版本', versions_list, 0, False)
+            self, "选择游戏版本", '请选择一个将要导出的游戏版本', versions_list, 0, False)
         self.game_version = version[0]
         if version[1]:
             self.pb_s2.setText(self.game_version)
@@ -90,10 +95,10 @@ class mmet(QMainWindow, Ui_MainWindow):
 
     def on_export(self):
         QMessageBox.information(
-            self, "准备导出", "按下 [OK] 以开始导出\n导出时可能会未响应,请耐心等待!")
-        game_folder_path = self.versions_path+os.sep+self.game_version
-        game_json_path = game_folder_path+os.sep+self.game_version+".json"
-        PCL_ini_path = game_folder_path+os.sep+"PCL"+os.sep+"Setup.ini"
+            self, "准备导出", "点击 [OK] 以开始导出\n导出时可能会未响应,请耐心等待!")
+        self.game_folder_path = self.versions_path+os.sep+self.game_version
+        game_json_path = self.game_folder_path+os.sep+self.game_version+".json"
+        PCL_ini_path = self.game_folder_path+os.sep+"PCL"+os.sep+"Setup.ini"
         if os.path.exists(PCL_ini_path):
             with open(PCL_ini_path, "r", encoding="utf-8") as f:
                 ini = f.readlines()
@@ -107,27 +112,27 @@ class mmet(QMainWindow, Ui_MainWindow):
                 VersionForge = ini[ini_forge_line].replace(
                     "VersionForge:", "").replace("\n", "")
                 if VersionFabric != "":
-                    loader_version = "fabric-"+VersionFabric
+                    self.loader_version = "fabric-"+VersionFabric
                 else:
-                    loader_version = "forge-"+VersionForge
+                    self.loader_version = "forge-"+VersionForge
             with open(game_json_path, "r", encoding="utf-8") as f:
                 game_json = json.load(f)
-                client_version = game_json["clientVersion"]
+                self.client_version = game_json["clientVersion"]
         else:
             try:
                 with open(game_json_path, "r", encoding="utf-8") as f:
                     game_json = json.load(f)
-                    loader_version = game_json["patches"][1]["id"] + \
+                    self.loader_version = game_json["patches"][1]["id"] + \
                         "-"+game_json["patches"][1]["version"]
             except:
-                loader_version = QInputDialog.getText(
-                    self, '输入mod加载器信息', '无法自动读取mod加载器信息\n请手动输入mod加载器名称及其版本\n例如: fabric-0.14.6')
+                self.loader_version = QInputDialog.getText(
+                    self, '输入mod加载器信息', '无法自动读取mod加载器信息<br>请手动输入mod加载器名称及其版本<br>例如: <strong>fabric-0.14.6</strong>')
             try:
-                client_version = game_json["patches"][0]["version"]
+                self.client_version = game_json["patches"][0]["version"]
             except:
-                client_version = QInputDialog.getText(
-                    self, '输入游戏版本信息', '无法自动读取游戏版本信息\n请手动输入游戏版本\n例如: 1.16.5')
-        # QMessageBox.information(self,"debug",f"loader_version:{loader_version}\nclient_version:{client_version}\nVersionFabric:{VersionFabric}\nVersionForge:{VersionForge}")
+                self.client_version = QInputDialog.getText(
+                    self, '输入游戏版本信息', '无法自动读取游戏版本信息<br>请手动输入游戏版本<br>例如: <strong>1.16.5</strong>')
+        # QMessageBox.information(self,"debug",f"loader_version:{self.loader_version}\nclient_version:{self.client_version}\nVersionFabric:{VersionFabric}\nVersionForge:{VersionForge}")
         e_config = self.cb_s3_config.isChecked()
         e_servers = self.cb_s3_servers.isChecked()
         e_options = self.cb_s3_options.isChecked()
@@ -135,37 +140,43 @@ class mmet(QMainWindow, Ui_MainWindow):
         e_saves = self.cb_s3_saves.isChecked()
         e_shaderpacks = self.cb_s3_shaderpacks.isChecked()
         e_all = self.cb_s3_all.isChecked()
-        manifest_json = {"minecraft": {"version": client_version, "modLoaders": [{"id": loader_version, "primary": True}]}, "manifestType": "minecraftModpack",
+        manifest_json = {"minecraft": {"version": self.client_version, "modLoaders": [{"id": self.loader_version, "primary": True}]}, "manifestType": "minecraftModpack",
                          "manifestVersion": 1, "name": self.game_version, "version": "1.0.0", "author": "MinecraftModpackExportTool "+getpass.getuser(), "files": [], "overrides": "overrides"}
-        os.mkdir("temp")
-        json.dump(manifest_json, open("temp"+os.sep +
+        os.mkdir("mmet_temp")
+        json.dump(manifest_json, open("mmet_temp"+os.sep +
                   "manifest.json", "w", encoding="utf-8"))
         if not e_all:
-            os.mkdir("temp"+os.sep+"overrides")
-            shutil.copytree(game_folder_path+os.sep+"mods",
-                            "temp"+os.sep+"overrides"+os.sep+"mods")
+            os.mkdir("mmet_temp"+os.sep+"overrides")
+            shutil.copytree(self.game_folder_path+os.sep+"mods",
+                            "mmet_temp"+os.sep+"overrides"+os.sep+"mods")
             if e_config:
-                shutil.copytree(game_folder_path+os.sep+"config",
-                                "temp"+os.sep+"overrides"+os.sep+"config")
+                shutil.copytree(self.game_folder_path+os.sep+"config",
+                                "mmet_temp"+os.sep+"overrides"+os.sep+"config")
             if e_servers:
-                shutil.copy(game_folder_path+os.sep+"servers.dat",
-                            "temp"+os.sep+"overrides")
+                shutil.copy(self.game_folder_path+os.sep+"servers.dat",
+                            "mmet_temp"+os.sep+"overrides")
             if e_options:
-                shutil.copy(game_folder_path+os.sep+"options.txt",
-                            "temp"+os.sep+"overrides")
+                shutil.copy(self.game_folder_path+os.sep+"options.txt",
+                            "mmet_temp"+os.sep+"overrides")
             if e_resourcepacks:
-                shutil.copytree(game_folder_path+os.sep +
-                                "resourcepacks", "temp"+os.sep+"overrides"+os.sep+"resourcepacks")
+                shutil.copytree(self.game_folder_path+os.sep +
+                                "resourcepacks", "mmet_temp"+os.sep+"overrides"+os.sep+"resourcepacks")
             if e_saves:
-                shutil.copytree(game_folder_path+os.sep+"saves",
-                                "temp"+os.sep+"overrides"+os.sep+"saves")
+                shutil.copytree(self.game_folder_path+os.sep+"saves",
+                                "mmet_temp"+os.sep+"overrides"+os.sep+"saves")
             if e_shaderpacks:
-                shutil.copytree(game_folder_path+os.sep+"shaderpacks",
-                                "temp"+os.sep+"overrides"+os.sep+"shaderpacks")
+                shutil.copytree(self.game_folder_path+os.sep+"shaderpacks",
+                                "mmet_temp"+os.sep+"overrides"+os.sep+"shaderpacks")
         else:
-            shutil.copytree(game_folder_path, "temp"+os.sep+"overrides")
-        make_zip("temp", self.game_version+".zip")
-        shutil.rmtree("temp")
+            shutil.copytree(self.game_folder_path, "mmet_temp"+os.sep+"overrides")
+        if os.path.exists("mmet_temp"+os.sep+"overrides"+os.sep+"config"+os.sep+"ias.json"):
+            r = QMessageBox.warning(
+                self, "盗号警告", "<h3>是否忽略ias.json?</h3><hr>由于你在整合包中使用了此mod:<br><strong>[游戏内账号切换]InGameAccountSwitcher</strong><br>你的账号登录信息会被储存在配置文件<strong>ias.json</strong>中<br><strong><font color=\"red\">如果你登录了正版，在整合包内保留将会导致别人登上你的账号！</font></strong>", QMessageBox.Yes | QMessageBox.No)
+            if r == QMessageBox.Yes:
+                os.remove("mmet_temp"+os.sep+"overrides" +
+                          os.sep+"config"+os.sep+"ias.json")
+        make_zip("mmet_temp", self.game_version+".zip")
+        shutil.rmtree("mmet_temp")
         QMessageBox.information(
             self, "导出整合包成功", "已导出整合包文件:\n"+os.path.abspath(self.game_version+".zip"))
 
@@ -174,13 +185,16 @@ class mmet(QMainWindow, Ui_MainWindow):
             self.on_export()
         except Exception as e:
             back = traceback.format_exc()
-            back=back.replace("\n","<br>").replace(" ","&nbsp;")
-            QMessageBox.critical(self, "导出失败", back+"<hr>你可以去此项目的 GitHub 上寻求帮助！")
-            shutil.rmtree("temp")
+            back = back.replace("\n", "<br>").replace(" ", "&nbsp;")
+            r=QMessageBox.critical(self, "导出失败", back +
+                                 "<hr>你可以去此项目的 GitHub 上寻求帮助！(点击 [Yes] 复制错误信息)",QMessageBox.Yes | QMessageBox.Ok)
+            if r==QMessageBox.Yes:
+                pyperclip.copy(back.replace("<br>","\n").replace("&nbsp;"," ")+f"\nclient_version: {self.client_version}\nloader_version: {self.loader_version}\ngame_folder_path: {self.game_folder_path}\nSystem: {platform.platform()}")
+            shutil.rmtree("mmet_temp")
 
     def show_about(self):
         QMessageBox.about(
-            self, "关于 MMET", "<strong>MinecraftModpackExportTool</strong><hr><p>作者: xxtg666</p><p>版本 1.0.4</p>")
+            self, "关于 MMET", "<strong>MinecraftModpackExportTool</strong><hr><p>作者: xxtg666</p><p>版本 1.0.5</p>")
 
     def open_github(self):
         wb.open("https://github.com/xxtg666/MinecraftModpackExportTool")
